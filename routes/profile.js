ce57b115963144f88e2a516cd807e3b6eecb4f78
ELISENWOLF <aravindprakash2001@gmail.com>
2021-11-29 20:35:52 +0530

    adding test

--- routes/profile.js
+++ routes/profile.js
@@ -3,10 +3,21 @@ import mongoose from "mongoose";
 const ObjectId = mongoose.Types.ObjectId;
 import { User } from "../route.js";
 // import session from 'express-session';
+import imgur from "imgur";
+const clientId = `2fb80e16e739310`;
+imgur.setClientId(clientId);
+imgur.setAPIUrl("https://api.imgur.com/3/upload");
+import fs from "fs";
+let rand = Math.random() * 1000;
+
 const db_string = `mongodb+srv://AravindPrakash:wowmaker@imgur.2l8iu.mongodb.net/imgur-b?retryWrites=true&w=majority`;
 const router = Router();
 router
   .get("/", (request, response) => {
+    // fs.writeFile(`${rand}.txt`, "Hello dummy content!", function (err) {
+    //   if (err) throw err;
+    //   console.log("Saved!");
+    // });
     const { user_id } = request.session;
     if (!request.session.user_id) {
       response.redirect("/");
